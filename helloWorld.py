3ddff6c11a50a097052f5893fbef44042efda850
Adwaiths1998 <66778855+Adwaiths1998@users.noreply.github.com>
2022-02-25 01:29:22 +0530

    Create helloWorld.py
    lesson 1

---
+++ helloWorld.py
@@ -0,0 +1,16 @@
+#printing
+print('hello World') 
+print("hello World") #works both time 
+
+#printing in different lines
+#1
+print('hello')
+print('world')
+
+#2
+print('hello','world',sep = "\n")
+
+#code in different lines but printing in single line
+print('hello',end=" ")
+print('world')
+
